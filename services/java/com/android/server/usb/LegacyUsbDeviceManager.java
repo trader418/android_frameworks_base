20e8b90d78725f0c742ffc35a4e6e2ff3072d92c
Oran Avraham <oranav@gmail.com>
2012-11-02 18:39:09 +0200

    UMS support with any volume, not only primary (2/3)
    This patch enables UMS support when any volume supports it, even when the
    primary storage doesn't.
    
    1. Vold: assign lun_numbers sequentially to the volumes being added to the
       VolumeManager.
    2. Fix portions of code which assume that the device has UMS support iff the
       primary storage supports UMS.
       Instead, determine whether the device supports UMS by checking each volume.
    3. Display the UMS option in Settings.apk when any volume supports it.
    
    Change-Id: I00a8338abe566f5fa20653733d13503064183c7e
    Signed-off-by: Oran Avraham <oranav@gmail.com>
    
    Conflicts:
    
    	services/java/com/android/server/MountService.java
    	services/java/com/android/server/usb/LegacyUsbDeviceManager.java
    	services/java/com/android/server/usb/UsbDeviceManager.java

--- services/java/com/android/server/usb/LegacyUsbDeviceManager.java
+++ services/java/com/android/server/usb/LegacyUsbDeviceManager.java
@@ -198,8 +198,8 @@ public class LegacyUsbDeviceManager extends UsbDeviceManager {
         StorageVolume[] volumes = storageManager.getVolumeList();
 
         if (volumes.length > 0) {
-            if (Settings.Secure.getInt(mContentResolver, Settings.Secure.USB_MASS_STORAGE_ENABLED, 0 ) == 1 ) {
-                massStorageSupported = volumes[0].allowMassStorage();
+            if (Settings.Secure.getInt(mContentResolver, Settings.Secure.USB_MASS_STORAGE_ENABLED, 0) == 1 ) {
+                massStorageSupported = storageManager.isUsbMassStorageSupported();
             } else {
                 massStorageSupported = false;
             }
