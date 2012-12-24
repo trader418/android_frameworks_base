1ebe99e77c8e0c8abd7631dee1d546e7211e2e5d
kufikugel <kufikugel@googlemail.com>
2012-12-24 03:09:11 +0100

    [Frameworks] Add reboot tile to QS
    and license change for PW reboot button
    
    Conflicts:
    
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -44,6 +44,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFI;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.cm.QSConstants.TILE_WIMAX;
+import static com.android.internal.util.cm.QSConstants.TILE_REBOOT;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -93,6 +94,7 @@ import com.android.systemui.quicksettings.VolumeTile;
 import com.android.systemui.quicksettings.WiFiDisplayTile;
 import com.android.systemui.quicksettings.WiFiTile;
 import com.android.systemui.quicksettings.WifiAPTile;
+import com.android.systemui.quicksettings.RebootTile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -238,6 +240,9 @@ public class QuickSettingsController {
                 qs = new QuietHoursTile(mContext, this);
             } else if (tile.equals(TILE_VOLUME)) {
                 qs = new VolumeTile(mContext, this, mHandler);
+            } else if (tile.equals(TILE_REBOOT)) {
+                qs = new RebootTile(mContext, inflater,
+                        (QuickSettingsContainerView) mContainerView, this);   
             } else if (tile.equals(TILE_EXPANDEDDESKTOP)) {
                 mTileStatusUris.add(Settings.System.getUriFor(Settings.System.EXPANDED_DESKTOP_STYLE));
                 if (QSUtils.expandedDesktopEnabled(resolver)) {
