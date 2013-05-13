a06cb1be9cdf4444df7c697849ffe18d63b0f908
Lars Greiss <kufikugel@googlemail.com>
2013-05-13 09:01:12 +0200

    Frameworks: Remove observer
    thanks to Jens....remove PieLayout observer
    completly
    
    cheers
    
    Change-Id: Icb722495b2d14c91b630f638f790dbcb14bbcef9
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/pie/PieLayout.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1939,7 +1939,7 @@ public abstract class BaseStatusBar extends SystemUI implements
             // destroy the pie container
             mPieContainer = null;
             // unregister listener and receiver
-            mPieController.destroyPie();
+            mPieController.detachContainer();
         }
     }
 
