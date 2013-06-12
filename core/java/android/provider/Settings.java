a0e9615cf6de64089589e5540fa6a663b6ded327
tonyp <tonyptonyp@gmail.com>
2013-06-12 23:33:45 +0200

    HALO: Option to pause the underlying app (1/2)
    - adds an option to pause the active app when using HALO
    - this is enabled by default for devices with low memory
    - (currently: <640MB RAM available to the kernel)
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	services/java/com/android/server/am/ActivityStack.java
    
    Change-Id: I09c90d615b1b2954293214d5e6107e2e2caab803
    
    Conflicts:
    
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2838,12 +2838,6 @@ public final class Settings {
         */
         public static final String EXPANDED_RING_MODE = "expanded_ring_mode";
 
-       /**
-         * HALO enabled, should default to 0 (HALO is disabled)
-         * @hide
-         */
-        public static final String HALO_ENABLED = "halo_enabled";
-
         /**
         * Notification Power Widget - Custom Torch Mode
         * @hide
@@ -2878,6 +2872,12 @@ public final class Settings {
          */
         public static final String STATUS_BAR_BATTERY = "status_bar_battery";
 
+	/**
+         * HALO enabled, should default to 0 (HALO is disabled)
+         * @hide
+         */
+        public static final String HALO_ENABLED = "halo_enabled";
+
  	/**
          * HALO, should default to 0 (no, do not show)
          * @hide
@@ -2902,7 +2902,14 @@ public final class Settings {
          */
         public static final String HALO_SIZE = "halo_size";
 
-        /** 
+	/**
+         * HALO pause activities?, defaults to 0 (no, do not pause) on devices which isLargeRAM() == true
+         * otherwise it defaults to 1 (yes, do pause)
+         * @hide
+         */
+        public static final String HALO_PAUSE = "halo_pause";
+
+        /**
          * Notifications Expand Behavior
          * @hide
          */
