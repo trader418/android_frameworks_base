55888db26e392937b9e522b349cf9f0ec03b2ada
drcmda <drcmda@gmail.com>
2013-08-04 17:33:51 -0300

    Halo full with 4.3 commits
    Inside:
    - HALO: cleanup: 4f4dd8dceaeafa182d75606f7c3adf59b37d973d
    - HALO: new drawables for hdpi and mdpi: d76f23efbc101e14d3423b8dd0645d64f7425555
    - HALO: unregister settings obeservers: 2048e663d2205ac304c8e5bc6da51b8cc442a12b
    - HALO: halo can save last position now: 05f4f5c55f5e8639ec03c4d3c701cfe9904b7e9c
    - HALO: Style revamp: f0ce74b71835e5912e52576bb503e88176f02883
    - HALO: hiddenstate-fix + smaller numberbatch: 7fae5c9df0f71194a2a59fcafd3610176fbc4623
    - HALO: cleanup & better drawables: 6ed0d9a613d0bf14ceeff141bc0650094b4323b5
    - HALO: hide bugfix, smaller numberbatch still: 1eaed47beb6286d37c78a95c841ba71d2ee02f8c
    - HALO: new vertical gestures (NOT READY FOR RELEASE): d54725ab41e239b6a6f096dcb19c69770749c7bd
    - HALO: clearall/silence (STILL NOT READY FOR RELEASE): 212b639759826fd53503534ee52add96ec367868
    - HALO: clearall-fix + dismiss icon (READY, BUT NEW STYLE IS XHDPI ONLY!): 6af90d1aca25fda9351ddea9caf5e4cd0df73e7a
    - HALO: cleanup & clearall-nap: 019cf36e9bee15c0b3701d8fa00509ad6dc1848f
    - HALO: Fix vertical gesture touch calculations: 8cf0d5c62d5fa6c5e8fa51b8273893d32c5b787e
    - HALO: bugfixes: 4a8c02330602a56d58f790165af0379ad2b1cbd9
    - HALO: Revert "Fix vertical gesture touch calculations": a4c65e878fbc3c37d6b18c1a9e07308de04c8311
    - HALO: Gesture Fix v2: 135d937eb2848c2ea2fe327165d255a1d8025d6e
    - HALO: Size (1/2): c3c4706f290fc96ac817337612a4f5605be55371
    - HALO: Avoid showing Halo))) icons in status bar while added to Halo: a404d6eaf9206fc658f050ccc2d974a589edfe3b
    - HALO: Completely hide Halo))) icons while tasking: 086962098bf1e966232dbdbf7f25759904274d3f
    - HALO: new transitions for number batch: fa86cc276e99b2679bcb310f8ca1ccb1b8aa4235
    - HALO: first_start tutorial & gesture dead-zone: 82ccafb7c53238742a8c70d9cffa3e586c93de47
    - HALO: NEW ICON SET (XHDPI ONLY!): f0bfc45e77b1b72e4ffd9d6e4f1f2e8235259cf1
    - HALO: dismiss/flip fix, clearall/last-notif-empty fix: 7106b09364bb250a532251cad1403da9b652f6de
    - HALO: first-run turorial less choppy + overlay: 7b995b44e1b4d5e2c8219de0831c1c257152e92f
    - HALO: bigger update: 5231f7da4568f017c7470c22c788f231118f574e
    - HALO: hdpi & mdpi icon update: 47c63c53907194210eb1541472cd2c88b48234d9
    - HALO: black toasts, italics, blue text: 3fd7c25b77b225036c68a502f9df575aba574eaf
    - HALO: batch envelope icon for non-numbered items: 28e7aacd9f526a1d7beb87774edc2b2948365d31
    - HALO: bugfixes: d41ff17fd420d3612255d10218bf7982324e3cda
    - HALO: vibration fix: 773d817f1abb960c943e60f56c82e23c688cd7fb
    - HALO: fix the integer division bug: 4ed9a25b414287b435cd5cba96603abe969187bc
    - HALO: batch message icon for hdpi and mdpi: ce9f94323c07f8dab5cd32208c5cd9afa7125863
    - HALO: what else, style changes: 586183976136707259385634d3daf8a083d9ecf8
    - HALO: fix speech fade out: 615f52bc7ad7701c1f6418e87a36b0f985707c3e
    - HALO: smoother operation: 6a67e60e985d6412a70ed87bd6eeca3f8271ff89
    - HALO: crash?: 6e1252dcb882c7bb0ebc81fc163cc24882b75ae3
    
    Change-Id: Ie705ccec63c706e366adb9acc356c80db52fc49d
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/cm_strings.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2891,6 +2891,12 @@ public final class Settings {
         public static final String HALO_HIDE = "halo_hide"; 
 
 	/**
+         * HALO size fraction, default is 1.0f (normal)
+         * @hide
+         */
+        public static final String HALO_SIZE = "halo_size";
+
+        /** 
          * Notifications Expand Behavior
          * @hide
          */
