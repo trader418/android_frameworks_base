90967ec3de4945e0669bca81d38841232f06d37a
Janson Kang <temasek71@gmail.com>
2013-08-05 09:43:14 +0800

    Ability to disable hardware keys via nav bar settings (2/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4009,6 +4009,27 @@ public final class Settings {
          */
         public static final String NAVIGATION_BAR_HEIGHT_LANDSCAPE = "navigation_bar_height_landscape";
 
+        /**
+         * Enable Menu key
+         *
+         * @hide
+         */
+        public static final String KEY_MENU_ENABLED = "key_menu_enabled";
+
+        /**
+         * Enable Back key
+         *
+         * @hide
+         */
+        public static final String KEY_BACK_ENABLED = "key_back_enabled";
+
+        /**
+         * Enable Home key
+         *
+         * @hide
+         */
+        public static final String KEY_HOME_ENABLED = "key_home_enabled";
+
     }
 
     /**
