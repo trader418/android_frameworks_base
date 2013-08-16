75708bfc042005889f00b99c6686ffcb1f033770
Der-Schubi <schubi@erlangen.ccc.de>
2013-08-16 08:18:32 -0300

    Option to hide network speed indicator when there is no traffic (1/2)
    Change-Id: Id77f746092100dd8dee9c3761f224a8167e56516
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Traffic.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3156,10 +3156,16 @@ public final class Settings {
         public static final String EXPANDED_DESKTOP_STYLE = "expanded_desktop_style";
 
         /**
+         * Hide network speed indicator when there is no traffic
+         * @hide
+         */
+        public static final String STATUS_BAR_TRAFFIC_HIDE = "status_bar_traffic_hide";
+
+        /**
          * Network speed indicator
          * @hide
          */
-        public static final String STATUS_BAR_TRAFFIC = "status_bar_traffic";
+        public static final String STATUS_BAR_TRAFFIC_ENABLE = "status_bar_traffic_enable";
 
 	/**
          * Sets color of statusbar traffic indicator
