fa865d3ceebfaa1cd75483f06ae4a3cc451f2f08
cristianomatos <cristianobmatos@gmail.com>
2013-09-04 07:49:18 -0300

    Frameworks: SlimPIE extended (1/2) - This commit also remove CyanogenMod Pie codes - All this implememtation is based on SlimRoms Pie, with small changes to fit our needs
    Thanks a bunch to Lars Greiss (kufikugel)
    
    Change-Id: I12a0509e6e3b820486691c17077dc7a3a26228dd
    
    Conflicts:
    
    	packages/SystemUI/res/values/colors.xml
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/pie/PieLayout.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/PieController.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -98,7 +98,6 @@ import android.widget.TextView;
 import android.service.notification.StatusBarNotification;
 
 import com.android.internal.statusbar.StatusBarIcon;
-import com.android.internal.util.pie.PiePosition;
 import com.android.systemui.EventLogTags;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.AppSidebar;
@@ -124,6 +123,7 @@ import com.android.systemui.statusbar.policy.NetworkController;
 import com.android.systemui.statusbar.policy.NotificationRowLayout;
 import com.android.systemui.statusbar.policy.OnSizeChangedListener;
 import com.android.systemui.statusbar.policy.Prefs;
+import com.android.systemui.statusbar.policy.PieController.Position; 
 
 public class PhoneStatusBar extends BaseStatusBar {
     static final String TAG = "PhoneStatusBar";
@@ -393,6 +393,8 @@ public class PhoneStatusBar extends BaseStatusBar {
                     Settings.System.NOTIFICATION_SHORTCUTS_HIDE_CARRIER), false, this, UserHandle.USER_ALL);
 	    resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.APP_SIDEBAR_POSITION), false, this, UserHandle.USER_ALL);
+	    resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.PIE_DISABLE_STATUSBAR_INFO), false, this);  
             update();
         }
 
@@ -611,7 +613,8 @@ public class PhoneStatusBar extends BaseStatusBar {
                 (NavigationBarView) View.inflate(context, R.layout.navigation_bar, null);
     
                 mNavigationBarView.setDisabledFlags(mDisabled);
-                mNavigationBarView.setBar(this); 
+                mNavigationBarView.setBar(this);
+		addNavigationBarCallback(mNavigationBarView);  
         }
 
         if (mRecreating) {
@@ -1604,13 +1607,19 @@ public class PhoneStatusBar extends BaseStatusBar {
 
     public void showClock(boolean show) {
 	if (mStatusBarView == null) return;
+	if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PIE_DISABLE_STATUSBAR_INFO, 0) == 1) {
+            show = false;
+        } 
         ContentResolver resolver = mContext.getContentResolver();
         View clock = mStatusBarView.findViewById(R.id.clock);
         View cclock = mStatusBarView.findViewById(R.id.center_clock);
         mShowClock = (Settings.System.getInt(resolver,
                 Settings.System.STATUS_BAR_CLOCK, 1) == 1);
-        boolean rightClock = (Settings.System.getInt(mContext.getContentResolver(), Settings.System.STATUSBAR_CLOCK_STYLE, 0) == 0);
-        boolean centerClock = (Settings.System.getInt(mContext.getContentResolver(), Settings.System.STATUSBAR_CLOCK_STYLE, 0) == 1);
+        boolean rightClock = (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUSBAR_CLOCK_STYLE, 0) == 0);
+        boolean centerClock = (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUSBAR_CLOCK_STYLE, 0) == 1); 
         if (rightClock && clock != null) {
             clock.setVisibility(show ? (mShowClock ? View.VISIBLE : View.GONE) : View.GONE);
         }
@@ -2622,19 +2631,17 @@ public class PhoneStatusBar extends BaseStatusBar {
         }
         propagateMenuVisibility(showMenu);
 
+        propagateMenuVisibility(showMenu);
+
         // hide pie triggers when keyguard is visible
         try {
             if (mWindowManagerService.isKeyguardLocked()) {
-                updatePieTriggerMask(PiePosition.BOTTOM.FLAG
-                        | PiePosition.TOP.FLAG);
+                disablePie(true);
             } else {
-                updatePieTriggerMask(PiePosition.LEFT.FLAG
-                        | PiePosition.BOTTOM.FLAG
-                        | PiePosition.RIGHT.FLAG
-                        | PiePosition.TOP.FLAG);
+                disablePie(false);
             }
         } catch (RemoteException e) {
-            // nothing else to do ...
+            // nothing else to do ... 
         }
 
         // See above re: lights-out policy for legacy apps.
