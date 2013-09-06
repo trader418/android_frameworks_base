ec45eb4cf644a5e937957f6a664c394f30e80581
Lars Greiss <kufikugel@googlemail.com>
2013-09-05 22:10:16 -0300

    Frameworks: Small correction snappoints
    I like easier and nicer code beside that it
    respects the reuse of snappoints better
    
    thanks jens
    
    cheers
    
    Change-Id: Iff4a1c107700b79c93df5463d524ef6e159ff387

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -2069,6 +2069,9 @@ public abstract class BaseStatusBar extends SystemUI implements
         int oldState = mPieTriggerSlots & mPieTriggerMask;
         mPieTriggerMask = newMask;
 
+	// pass actual trigger mask and slots to the attached container
+        mPieContainer.setSnapPoints(mPieTriggerMask & ~mPieTriggerSlots); 
+
         // first we check, if it would make a change
         if ((mPieTriggerSlots & mPieTriggerMask) != oldState
                 || mForceDisableBottomAndTopTrigger) {
@@ -2080,9 +2083,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     // This should only be called, when is is clear that the pie controls are active
     private void refreshPieTriggers() {
-	// pass actual trigger mask and slots to the attached container
-        mPieContainer.setPieTriggers(mPieTriggerMask, mPieTriggerSlots); 
-        for (Position g : Position.values()) {
+	for (Position g : Position.values()) {
             View trigger = mPieTrigger[g.INDEX];
             if (trigger == null && (mPieTriggerSlots & mPieTriggerMask & g.FLAG) != 0) {
                 trigger = new View(mContext);
