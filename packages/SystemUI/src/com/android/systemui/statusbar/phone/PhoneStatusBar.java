6e65b5783091cb49ffd5c6c138692adee3de5afa
Janson Kang <temasek71@gmail.com>
2013-09-12 18:16:06 +0800

    Fix typo

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3057,7 +3057,6 @@ public class PhoneStatusBar extends BaseStatusBar {
 		try {
                     // position app sidebar on left if in landscape orientation and device has a navbar
                     if (mWindowManagerService.hasNavigationBar() && 
-                            isDevicePhone(mContext) &&
                             config.orientation == Configuration.ORIENTATION_LANDSCAPE) {
                         mWindowManager.updateViewLayout(mAppSidebar,
                                 getAppSidebarLayoutParams(AppSidebar.SIDEBAR_POSITION_LEFT));
