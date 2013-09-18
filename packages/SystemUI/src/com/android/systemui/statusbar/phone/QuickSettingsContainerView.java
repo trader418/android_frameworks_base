64a6bd999222195c517e0804869849cb04b3a07e
Janson Kang <temasek71@gmail.com>
2013-09-19 07:50:26 +0800

    Revert "quicksettings columns: respect landscape mode"
    This reverts commit ed2f7886bc21f57c36d15bc8a9c7a0ad058f5503.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsContainerView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsContainerView.java
@@ -19,7 +19,6 @@ package com.android.systemui.statusbar.phone;
 import android.animation.LayoutTransition;
 import android.provider.Settings;
 import android.content.Context;
-import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
@@ -61,9 +60,7 @@ public class QuickSettingsContainerView extends FrameLayout {
     void updateResources() {
         Resources r = getContext().getResources();
         mCellGap = r.getDimension(R.dimen.quick_settings_cell_gap);
-        boolean iSlandscape =
-                getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
-        mNumColumns = iSlandscape ? 6 : Settings.System.getInt(getContext().getContentResolver(),
+        mNumColumns = Settings.System.getInt(getContext().getContentResolver(),
                 Settings.System.QUICK_SETTINGS_COLUMNS, 3);
         requestLayout();
     }
