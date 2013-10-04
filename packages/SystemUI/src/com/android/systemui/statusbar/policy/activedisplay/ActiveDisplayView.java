bc2bc123f8f0f72af28118488e7c35e30d8bf68e
Schubi <schubi@erlangen.ccc.de>
2013-10-04 18:00:01 +0200

    Active display: Add some log output
    Change-Id: I3443214fecb141a4df177b9cfffbb3a733257a5e
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -685,7 +685,8 @@ public class ActiveDisplayView extends FrameLayout {
     }
 
     private void turnScreenOff() {
-	mWakedByPocketMode = false; 
+	mWakedByPocketMode = false;
+	Log.i(TAG, "ActiveDisplay: turn screen off");  
         try {
             mPM.goToSleep(SystemClock.uptimeMillis(), 0);
         } catch (RemoteException e) {
@@ -703,6 +704,7 @@ public class ActiveDisplayView extends FrameLayout {
 
     private void enableProximitySensor() {
         if (mPocketModeEnabled && mDisplayNotifications) {
+	    Log.i(TAG, "ActiveDisplay: enable ProximitySensor"); 
             mProximityIsFar = true;
             registerSensorListener(mProximitySensor);
         }
@@ -710,6 +712,7 @@ public class ActiveDisplayView extends FrameLayout {
 
     private void disableProximitySensor() {
         if (mPocketModeEnabled && mDisplayNotifications) {
+	    Log.i(TAG, "ActiveDisplay: disable ProximitySensor"); 
             unregisterSensorListener(mProximitySensor);
         }
     } 
@@ -799,6 +802,7 @@ public class ActiveDisplayView extends FrameLayout {
 
     private void registerCallbacks() {
         if (!mCallbacksRegistered) {
+	    Log.i(TAG, "ActiveDisplay: register callbacks"); 
             registerBroadcastReceiver();
             registerNotificationListener();
             mCallbacksRegistered = true;
@@ -807,6 +811,7 @@ public class ActiveDisplayView extends FrameLayout {
 
     private void unregisterCallbacks() {
         if (mCallbacksRegistered) {
+	    Log.i(TAG, "ActiveDisplay: unregister callbacks"); 
             unregisterBroadcastReceiver();
             unregisterNotificationListener();
             mCallbacksRegistered = false;
@@ -959,6 +964,7 @@ public class ActiveDisplayView extends FrameLayout {
             mPM.wakeUp(SystemClock.uptimeMillis());
         } catch (RemoteException e) {
         }
+	Log.i(TAG, "ActiveDisplay: Wake device"); 
         updateTimeoutTimer();
     }
 
@@ -1087,6 +1093,7 @@ public class ActiveDisplayView extends FrameLayout {
                     if (!isScreenOn() && mPocketModeEnabled && !isOnCall() && !inQuietHours()) {
                         if (System.currentTimeMillis() >= (mPocketTime + POCKET_THRESHOLD)) {
                             mWakedByPocketMode = true;
+			    Log.i(TAG, "ActiveDisplay: waked by Pocketmode"); 
 
                             mNotification = getNextAvailableNotification();
                             if (mNotification != null) showNotification(mNotification, true);
@@ -1097,6 +1104,7 @@ public class ActiveDisplayView extends FrameLayout {
                     mProximityIsFar = false;
 		    if (isScreenOn() && mPocketModeEnabled && !isOnCall() && mWakedByPocketMode) {
                         mWakedByPocketMode = false;
+			Log.i(TAG, "ActiveDisplay: sent to sleep by Pocketmode"); 
 
                         restoreBrightness();
                         cancelTimeoutTimer();
