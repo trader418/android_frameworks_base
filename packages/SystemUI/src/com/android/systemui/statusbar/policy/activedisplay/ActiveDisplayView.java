b273ec3fb48d488d6a1b750cd4b84559d96670e4
Janson Kang <temasek71@gmail.com>
2013-10-21 16:57:59 +0800

    Revert "Active display: Catch NPE"
    This reverts commit 066dc6fbeb9e3b466736810a74dbabd982107356.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -858,8 +858,6 @@ public class ActiveDisplayView extends FrameLayout {
                 } catch (RemoteException re) {
                 } catch (NameNotFoundException nnfe) {
                 } catch (Resources.NotFoundException e) {
-                } catch (NullPointerException npe) {
-                // Do nothing
                 }
             }
         });
@@ -1002,8 +1000,6 @@ public class ActiveDisplayView extends FrameLayout {
             });
         } catch (NameNotFoundException e) {
         } catch (Resources.NotFoundException e) {
-        } catch (NullPointerException npe) {
-        // Do nothing
         }
     }
 
