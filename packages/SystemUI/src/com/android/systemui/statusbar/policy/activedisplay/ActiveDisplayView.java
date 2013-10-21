f61d7fdf5baf91aed7b972b1717785f23b403e25
Janson Kang <temasek71@gmail.com>
2013-10-21 16:57:32 +0800

    Revert "Active display: Next try to fix "turn off display if device gets pocketed again""
    This reverts commit 87385eb921add1845e947fae1f29a170ab7cf4b6.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -625,7 +625,6 @@ public class ActiveDisplayView extends FrameLayout {
         }
         setVisibility(View.GONE);
         restoreBrightness();
-	mWakedByPocketMode = false; 
         mBar.disable(0);
         cancelTimeoutTimer();
         if (mLightSensor != null)
