282b50dbd52922c0f1aece312b98c12fa865c63a
Janson Kang <temasek71@gmail.com>
2013-10-21 17:02:11 +0800

    Revert "Active display: Ability to turn off the screen, when a not clearable notification is present"
    This reverts commit dbcc4e482b2e06ba37bd5c25db501cfd79d4f4f2.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -532,7 +532,7 @@ public class ActiveDisplayView extends FrameLayout {
             if (mNotification != null && mNotification.isClearable()) {
                 storedDraw.add(new TargetDrawable(res, res.getDrawable(R.drawable.ic_ad_dismiss_notification)));
             } else {
-                storedDraw.add(new TargetDrawable(res, res.getDrawable(R.drawable.ic_qs_power)));
+                storedDraw.add(new TargetDrawable(res, null));
             }
         }
         storedDraw.add(new TargetDrawable(res, null));
@@ -644,19 +644,17 @@ public class ActiveDisplayView extends FrameLayout {
     }
 
     private void handleDismissNotification() {
-        if (mNotification != null && mNotification.isClearable()) {
-           try {
-                mNM.cancelNotificationFromListener(mNotificationListener,
-                        mNotification.getPackageName(), mNotification.getTag(),
-                        mNotification.getId());
-            } catch (RemoteException e) {
-            }
-            mNotification = getNextAvailableNotification();
-            if (mNotification != null) {
-                setActiveNotification(mNotification, true);
-                userActivity();
-                return;
-            }
+        try {
+            mNM.cancelNotificationFromListener(mNotificationListener,
+                    mNotification.getPackageName(), mNotification.getTag(),
+                    mNotification.getId());
+        } catch (RemoteException e) {
+        }
+        mNotification = getNextAvailableNotification();
+        if (mNotification != null) {
+            setActiveNotification(mNotification, true);
+            userActivity();
+            return;
         }
 
         // no other notifications to display so turn screen off
