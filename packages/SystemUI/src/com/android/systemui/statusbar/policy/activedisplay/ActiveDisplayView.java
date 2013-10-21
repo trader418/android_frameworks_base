ff11dee47ede69729ed0da9d2686e5129efb6e4f
Janson Kang <temasek71@gmail.com>
2013-10-21 16:56:11 +0800

    Revert "Active display: Only fetch next notification if mNotification is null"
    This reverts commit 4724bff4c903053140226655f9827c2ba6252de3.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -1096,9 +1096,7 @@ public class ActiveDisplayView extends FrameLayout {
                             mWakedByPocketMode = true;
 			    Log.i(TAG, "ActiveDisplay: waked by Pocketmode"); 
 
-                            if (mNotification == null) {
-                                mNotification = getNextAvailableNotification();
-                            }
+                            mNotification = getNextAvailableNotification();
                             if (mNotification != null) showNotification(mNotification, true);
                         }
                     }
@@ -1142,9 +1140,7 @@ public class ActiveDisplayView extends FrameLayout {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (ACTION_REDISPLAY_NOTIFICATION.equals(action)) {
-                if (mNotification == null) {
-                    mNotification = getNextAvailableNotification();
-                }
+                mNotification = getNextAvailableNotification();
                 if (mNotification != null) showNotification(mNotification, true);
             } else if (ACTION_DISPLAY_TIMEOUT.equals(action)) {
                 turnScreenOff();
@@ -1153,9 +1149,7 @@ public class ActiveDisplayView extends FrameLayout {
             } else if (Intent.ACTION_SCREEN_ON.equals(action)) {
                 onScreenTurnedOn();
             } else if (ACTION_FORCE_DISPLAY.equals(action)) {
-                if (mNotification == null) {
-                    mNotification = getNextAvailableNotification();
-                }
+                mNotification = getNextAvailableNotification();
                 if (mNotification != null) showNotification(mNotification, true);
                 restoreBrightness();
             }
