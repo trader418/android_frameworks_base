f4ed0a5e64631adfb2610a7add99b3515b35efdf
0xD34D <clark@scheffsblend.com>
2013-10-28 16:27:53 -0200

    Add network usage stats to status bar. [1/2] PS2: Get the time delta between updates and use that to show average bytes per second.
    Change-Id: I63771220333a32063f675e6fc38c9aadb6de4274
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout/status_bar.xml
    	packages/SystemUI/res/values/dimens.xml
    	packages/SystemUI/res/values/styles.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3282,6 +3282,18 @@ public final class Settings {
         public static final String NOTIFICATIONS_BEHAVIOUR = "notifications_behaviour"; 
 
         /**
+         * Whether to show the network status in the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_STATS = "status_bar_network_stats";
+ 
+        /**
+         * Frequency at which stats are updated, in milliseconds
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL = "status_bar_network_stats_update_frequency";
+
+        /**
          * Circle battery icon color
          * in statusbar
          */
