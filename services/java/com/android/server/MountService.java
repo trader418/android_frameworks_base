da8367cfce1570516b121663d00d813455d14f0a
Scott Mertz <scott@cyngn.com>
2014-11-19 13:56:40 -0800

    ums: support UMS on any volume
    Change-Id: I43668a5200d30cef953e0d3e9ded2d8cf0cbbd22

--- services/java/com/android/server/MountService.java
+++ services/java/com/android/server/MountService.java
@@ -1399,10 +1399,14 @@ class MountService extends IMountService.Stub
         mContext.registerReceiver(mUserReceiver, userFilter, null, mHandler);
 
         // Watch for USB changes on primary volume
-        final StorageVolume primary = getPrimaryPhysicalVolume();
-        if (primary != null && primary.allowMassStorage()) {
-            mContext.registerReceiver(
-                    mUsbReceiver, new IntentFilter(UsbManager.ACTION_USB_STATE), null, mHandler);
+        final StorageVolume[] storageVolumes = getVolumeList();
+        for(StorageVolume volume : storageVolumes) {
+            if (volume.allowMassStorage()) {
+                mContext.registerReceiver(
+                        mUsbReceiver,
+                        new IntentFilter(UsbManager.ACTION_USB_STATE), null, mHandler);
+                break;
+            }
         }
 
         // Watch for idle maintenance changes
@@ -1583,9 +1587,6 @@ class MountService extends IMountService.Stub
     public void setUsbMassStorageEnabled(boolean enable) {
         waitForReady();
         validatePermission(android.Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS);
-
-        final StorageVolume primary = getPrimaryPhysicalVolume();
-        if (primary == null) return;
 
         // TODO: Add support for multiple share methods
 
