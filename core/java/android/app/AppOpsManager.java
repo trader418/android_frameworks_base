05220085f34a1bf9fe9f26afd210558a8341db75
Sam Mortimer <sam@mortimer.me.uk>
2014-11-25 13:16:43 -0800

    [2/2] Framework AppOps: Add per Op allowed and ignored counters
    Port from cm-10.2 and cm-11.0
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Wed Oct 2 22:06:42 2013 -0700
    [2/2] AppOps: Add per Op allowed and ignored counters
    Change-Id: Ifb3d25e17dbab082b816a0a655b4796a83af336b
    
    Change-Id: Ifb3d25e17dbab082b816a0a655b4796a83af336b
    
    Patchset: 2
    http://review.cyanogenmod.org/#/c/79395/

--- core/java/android/app/AppOpsManager.java
+++ core/java/android/app/AppOpsManager.java
@@ -1336,13 +1336,18 @@ public class AppOpsManager {
         private final long mTime;
         private final long mRejectTime;
         private final int mDuration;
+        private final int mAllowedCount;
+        private final int mIgnoredCount;
 
-        public OpEntry(int op, int mode, long time, long rejectTime, int duration) {
+        public OpEntry(int op, int mode, long time, long rejectTime, int duration,
+                int allowedCount, int ignoredCount) {
             mOp = op;
             mMode = mode;
             mTime = time;
             mRejectTime = rejectTime;
             mDuration = duration;
+            mAllowedCount = allowedCount;
+            mIgnoredCount = ignoredCount;
         }
 
         public int getOp() {
@@ -1369,6 +1374,14 @@ public class AppOpsManager {
             return mDuration == -1 ? (int)(System.currentTimeMillis()-mTime) : mDuration;
         }
 
+        public int getAllowedCount() {
+            return mAllowedCount;
+        }
+
+        public int getIgnoredCount() {
+            return mIgnoredCount;
+        }
+
         @Override
         public int describeContents() {
             return 0;
@@ -1381,6 +1394,8 @@ public class AppOpsManager {
             dest.writeLong(mTime);
             dest.writeLong(mRejectTime);
             dest.writeInt(mDuration);
+            dest.writeInt(mAllowedCount);
+            dest.writeInt(mIgnoredCount);
         }
 
         OpEntry(Parcel source) {
@@ -1389,6 +1404,8 @@ public class AppOpsManager {
             mTime = source.readLong();
             mRejectTime = source.readLong();
             mDuration = source.readInt();
+            mAllowedCount = source.readInt();
+            mIgnoredCount = source.readInt();
         }
 
         public static final Creator<OpEntry> CREATOR = new Creator<OpEntry>() {
@@ -1931,4 +1948,11 @@ public class AppOpsManager {
         }
     }
 
+    /** @hide */
+    public void resetCounters() {
+        try {
+            mService.resetCounters();
+        } catch (RemoteException e) {
+        }
+    }
 }
