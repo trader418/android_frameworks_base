c185c83eebb1a3df9ed5ed414084730198de3894
Yashar Ahmedizadeh <yashade2001@gmail.com>
2014-11-27 19:59:30 +0200

    remove search bar in recents menu
    Signed-off-by: Yashar Ahmedizadeh <yashade2001@gmail.com>
    (cherry picked from commit 70d12420053e7d5277c9395a2abacf3f63bad07a)

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -250,9 +250,9 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
             }
             findViewById(R.id.clear_recents).setVisibility(View.VISIBLE);
             if (mRecentsView.hasSearchBar()) {
-                mRecentsView.setSearchBarVisibility(View.VISIBLE);
+                mRecentsView.setSearchBarVisibility(View.GONE);
             } else {
-                addSearchBarAppWidgetView();
+                //addSearchBarAppWidgetView();
             }
         }
 
