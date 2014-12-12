2aa8332dbc9dd4159a486879ab2f89156d23253b
Janson Kang <temasek71@gmail.com>
2014-12-12 17:57:50 +0800

    Revert "Battery text mode (1/2)"
    This reverts commit 6d48ca1e0516090d47ee013dd6d504607b65b952.
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout/status_bar.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3307,12 +3307,6 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
         /**
-         * Status bar battery % text
-         * @hide
-         */
-        public static final String STATUS_BAR_SHOW_BATTERY_TEXT = "status_bar_show_battery_text";
-
-        /**
          * Volume music controls
          * @hide
          */
