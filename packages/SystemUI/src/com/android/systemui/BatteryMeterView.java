b0cf848e745435c04d9ea91b7fa73879122322f3
Michael Bestas <mikeioannina@gmail.com>
2014-12-31 08:01:19 +0800

    Cleanup battery style code (Updated to PS13)
    * Cleanup commented out code
    * Fix landscape battery icon padding
    * Fix landscape battery icon charging indicator
    * Fix landscape battery icon percentage indicator
    * Fix circle battery frame color on some black backgrounds
    * Fix circle battery artifact on low percentage
    * Fix percentage not shown while charging
    * Fix percentage always showing on expanded status bar
    * Fix battery icon showing after reboot on text/hidden modes
    
    Change-Id: Ic56878c26e9a3416149eaaa8a9325d56d664e22e

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -20,6 +20,7 @@ package com.android.systemui;
 import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Resources;
@@ -40,7 +41,6 @@ import android.util.DisplayMetrics;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-import android.content.ContentResolver;
 
 import com.android.systemui.statusbar.policy.BatteryController;
 
