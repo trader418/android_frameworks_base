trader418/android_frameworks_base

commit ea3445d1f70eb6fc01e2adb872f2077d678b9fad

Themes: Handle case when keyguard is showing during theme change
After we recreate the status bar we need to check if the keyguard
was showing and if so make sure it is showing after the theme
change.  We also need to make sure the phone status bar is not
visible if the keyguard was showing, otherwise you see two status
bars.

Change-Id: Iba09e3e5c8cf52d49f292423a50034a3cf84d9c1

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3777,6 +3777,17 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         });
         // restart the keyguard so it picks up the newly created ScrimController
         startKeyguard();
+
+        // if the keyguard was showing while this change occurred we'll need to do some extra work
+        if (mState == StatusBarState.KEYGUARD) {
+            // this will make sure the keyguard is showing
+            showKeyguard();
+            // The following views need to be invisible if the keyguard is showing
+            // These views were hidden but re-inflating the status bar changed them back to visible
+            mClockView.setVisibility(View.INVISIBLE);
+            mNotificationIconArea.setVisibility(View.INVISIBLE);
+            mSystemIconArea.setVisibility(View.INVISIBLE);
+        }
     }
 
     private void removeAllViews(ViewGroup parent) {

commit 25874dac8f20f8bfd19e4d122d040bcec234f435

Fix build broke by theme commit
Introduced in https://github.com/temasek/android_frameworks_base/commit/ea3445d1f70eb6fc01e2adb872f2077d678b9fad

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3784,7 +3784,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             showKeyguard();
             // The following views need to be invisible if the keyguard is showing
             // These views were hidden but re-inflating the status bar changed them back to visible
-            mClockView.setVisibility(View.INVISIBLE);
+            mCenterClockLayout.setVisibility(View.INVISIBLE);
             mNotificationIconArea.setVisibility(View.INVISIBLE);
             mSystemIconArea.setVisibility(View.INVISIBLE);
         }