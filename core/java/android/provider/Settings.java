1d280f89e700846ab584a0a02c78b6bae86fc23b
Janson Kang <temasek71@gmail.com>
2015-02-05 08:24:24 +0800

    [SQUASH] Remove Notification Ticker [1/2]
    Not working and one of causes of SystemUI FC

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2240,13 +2240,6 @@ public final class Settings {
         public static final String QS_SMART_PULLDOWN = "qs_smart_pulldown";
 
         /**
-         * Whether to enable ticker animation in statusbar
-         *
-         * @hide
-         */
-        public static final String STATUS_BAR_TICKER_ENABLED = "status_bar_ticker_enabled";
-
-        /**
          * Deprecated Use {@link android.provider.Settings.Secure.QS_TILES}
          * @hide
          */
