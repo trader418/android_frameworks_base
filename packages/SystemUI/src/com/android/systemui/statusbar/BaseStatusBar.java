e85fc17c192018c66994da50af402b3e6a4c47eb
Sam Mortimer <sam@mortimer.me.uk>
2015-02-09 17:30:48 -0800

    Don't lookup application keyguard notification visibility unnecessarily
    Change-Id: I7a70f2c9d9e813c71544f2446c9bc6502f061c4f

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1832,6 +1832,9 @@ public abstract class BaseStatusBar extends SystemUI implements
     }
 
     private boolean shouldShowOnKeyguard(StatusBarNotification sbn) {
+        if (!mShowLockscreenNotifications || mNotificationData.isAmbient(sbn.getKey())) {
+            return false;
+        }
         final int showOnKeyguard = mNoMan.getShowNotificationForPackageOnKeyguard(
                 sbn.getPackageName(), sbn.getUid());
         boolean isKeyguardAllowedForApp =
@@ -1840,8 +1843,7 @@ public abstract class BaseStatusBar extends SystemUI implements
             isKeyguardAllowedForApp =
                     (showOnKeyguard & Notification.SHOW_NO_ONGOING_NOTI_ON_KEYGUARD) == 0;
         }
-        return mShowLockscreenNotifications && !mNotificationData.isAmbient(sbn.getKey())
-                && isKeyguardAllowedForApp;
+        return isKeyguardAllowedForApp;
     }
 
     protected void setZenMode(int mode) {
